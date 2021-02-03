944e34db0867fb17eec7b2de33b5b3689ed3276a
Michal Staniaszek <m.staniaszek@gmail.com>
2021-02-03 12:30:47 +0000

    fix a bad merge

--- src/procman_ros/procman_deputy.cpp
+++ src/procman_ros/procman_deputy.cpp
@@ -837,12 +837,8 @@ static void usage() {
 using namespace procman;
 
 int main(int argc, char **argv) {
-<<<<<<< HEAD
   ros::init(argc, argv, "procman_ros_deputy");
   const char *optstring = "hvfl:i:np";
-=======
-  const char *optstring = "hvfl:i:n";
->>>>>>> master
   int c;
   bool start_roscore = true;
   bool persist_roscore = false;
@@ -929,7 +925,6 @@ int main(int argc, char **argv) {
   if (start_roscore && !ros::master::check()) {
     pid_t pid = fork();
     if (pid == 0) {
-<<<<<<< HEAD
       // redirect output so that it doesn't show on the terminal  
       std::string roscore_cmd = "roscore > /dev/null 2>&1";
 
@@ -944,16 +939,6 @@ int main(int argc, char **argv) {
       }
 
       int ignored = system(roscore_cmd.c_str());
-=======
-      // child process changes its process group and then runs a roscore.
-      // Changing the process group is necessary so that it doesn't receive
-      // sigint from the terminal
-      setpgid(0, 0);
-      // redirect output so that it doesn't show on the terminal, and also
-      // background the process so that this child process of deputy exits
-      // immediately after spawning the roscore
-      int ret = system("roscore > /dev/null 2>&1 &");
->>>>>>> master
       exit(0);
     }
     // parent process continues and runs the deputy, but the two processes are
