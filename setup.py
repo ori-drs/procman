423e092e32c3204456a08738765257b976588d14
Benoit Casseau <benoit@oxfordrobotics.institute>
2023-06-01 16:29:15 +0100

    convert python files to ros2

--- setup.py
+++ setup.py
@@ -1,11 +1,44 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-
+import os
+from glob import glob
 from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['procman_ros', 'procman_ros.sheriff_gtk'],
-    package_dir={'': 'python/src'})
+package_name = 'procman_ros'
 
-setup(**setup_args)
+setup(
+    name=package_name,
+    version='0.0.0',
+    # Packages to export
+    packages=[package_name],
+    # Files we want to install, specifically launch files
+    data_files=[
+        # Install marker file in the package index
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        # Include our package.xml file
+        (os.path.join('share', package_name), ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    # This is important as well
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='ROS 2 Developer',
+    author_email='ros2@ros.com',
+    maintainer='ROS 2 Developer',
+    maintainer_email='ros2@ros.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: BSD-3',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Procman_ros',
+    license='BSD-3',
+    # Like the CMakeLists add_executable macro, you can add your python
+    # scripts here.
+    entry_points={
+        'console_scripts': [
+            'sheriff = procman_ros.sheriff_gtk:main'
+        ],
+    },
+)
